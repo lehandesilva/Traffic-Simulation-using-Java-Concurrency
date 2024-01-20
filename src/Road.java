fdea3005a44a5b9ddd3d5cb9c621e19e6043aeaa
NumeralArcher <lehande28@gmail.com>
2024-01-20 12:09:28 +0000

    build: logic for add car

--- src/Road.java
+++ src/Road.java
@@ -1,21 +1,31 @@
 import java.util.concurrent.Semaphore;
 
 public class Road {
-    private Semaphore mutex = new Semaphore(1);
+    private Semaphore mutex;
     //private Semaphore full = new Semaphore(0);
     private final int roadSize;
     //private Semaphore empty = new Semaphore(roadSize);
-    public Road(int roadSize) {
+    private string entryPoint;
+    private string destination;
+    private Vehicle[] cars;
+
+    public Road(int roadSize, string entry,string destination) {
+        this.mutex = new Semaphore(1);
         this.roadSize = roadSize;
-        int [] roadQueue = new int[roadSize];
+        this.cars = new Vehicle[roadSize];
+        this.entryPoint = entry;
+        this.destination = destination;
     }
     //method to check if space avail
 
     //method to add vehicle (produce)
-    public void addVehicle() {
-        for (int i = 0; i < roadSize; i++) {
+    public void addVehicle(Vehicle car) throws InterruptedException {
+        mutex.acquire();
+        /*
+        * Check if there is space available
+        * add the car to the array
+        * release the mutex*/
 
-        }
     }
 
     //method to remove vehicle (consume)
