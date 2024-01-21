e460dbbed68d8bb6520b2858787357a80d4241a2
NumeralArcher <lehande28@gmail.com>
2024-01-21 02:22:44 +0000

    build: logic for road class implemented

--- src/Road.java
+++ src/Road.java
@@ -4,33 +4,74 @@ public class Road {
     private Semaphore mutex;
     //private Semaphore full = new Semaphore(0);
     private final int roadSize;
-    //private Semaphore empty = new Semaphore(roadSize);
-    private string entryPoint;
-    private string destination;
+    private String entryPoint;
+    private String destination;
     private Vehicle[] cars;
+    private int frontPointer;
+    private int rearPointer;
+    private int count;
 
-    public Road(int roadSize, string entry,string destination) {
+    public Road(int roadSize, String entry,String destination) {
         this.mutex = new Semaphore(1);
         this.roadSize = roadSize;
         this.cars = new Vehicle[roadSize];
         this.entryPoint = entry;
         this.destination = destination;
+        this.frontPointer = -1;
+        this.rearPointer = -1;
+        this.count = 0;
+
     }
     //method to check if space avail
-
+    public boolean isFull() {
+        if (frontPointer ==0 && rearPointer == roadSize -1){
+            return true;
+        } else if (frontPointer == rearPointer + 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public boolean isEmpty() {
+        if (frontPointer == -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     //method to add vehicle (produce)
-    public void addVehicle(Vehicle car) throws InterruptedException {
+    public boolean addVehicle(Vehicle car) throws InterruptedException {
         mutex.acquire();
+        if (frontPointer == -1) {
+            frontPointer = 0;
+        }
+        rearPointer = (rearPointer + 1) % roadSize;
+        cars[rearPointer] = car;
+        count++;
+        mutex.release();
+        return true;
         /*
         * Check if there is space available
         * add the car to the array
         * release the mutex*/
-
     }
 
     //method to remove vehicle (consume)
-    public void removeVehicle() {
-
+    public Vehicle removeVehicle() throws InterruptedException{
+        mutex.acquire();
+        Vehicle removedCar;
+        removedCar = cars[frontPointer];
+        if (frontPointer == rearPointer) {
+            frontPointer = -1;
+            rearPointer = -1;
+        } else {
+            frontPointer = (frontPointer + 1) % roadSize;
+        }
+        count--;
+        mutex.release();
+        return removedCar;
     }
 
 }
