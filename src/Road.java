bea4cb16382814f41d341ffe9c65db42f7d94548
lehan <lehande28@gmail.com>
2024-02-02 12:07:21 +0000

    build: seperated acquiring and releasing mutex to its own functions

--- src/Road.java
+++ src/Road.java
@@ -9,11 +9,9 @@ public class Road {
     private String[] couldBeReachedArray;
     private int frontPointer;
     private int rearPointer;
+    private int count;
     private final Semaphore emptySlots;
     private final Semaphore occupiedSlots;
-
-
-
     public Road(int roadSize, String entry,String destination, String[] couldBeReachedArray) {
         this.mutex = new Semaphore(1);
         this.roadSize = roadSize;
@@ -25,6 +23,7 @@ public class Road {
         this.couldBeReachedArray = couldBeReachedArray;
         this.emptySlots = new Semaphore(roadSize);
         this.occupiedSlots = new Semaphore(0);
+        this.count = 0;
     }
     //method to check if space avail
     public boolean isFull() {
@@ -45,19 +44,29 @@ public class Road {
             return false;
         }
     }
-    //method to add vehicle (produce)
-    public void addVehicle(Vehicle car) throws InterruptedException {
-        emptySlots.acquire();
-        System.out.println("Thread " + Thread.currentThread().getName()+ "acquired empty");
+    public void acquireMutex() throws InterruptedException {
+        System.out.println("Trying to acquire mutex " + Thread.currentThread().getName() + " count: " + count);
         mutex.acquire();
         System.out.println("Thread " + Thread.currentThread().getName()+ "acquired mutex");
+    }
+    public void releaseMutex() throws InterruptedException {
+        System.out.println("Trying to release mutex " + Thread.currentThread().getName() + " count: " + count);
+        mutex.release();
+        System.out.println("Thread " + Thread.currentThread().getName()+ "released mutex");
+    }
+    //method to add vehicle (produce)
+    public void addVehicle(Vehicle car) throws InterruptedException {
+        System.out.println("Trying to acquire empty " + Thread.currentThread().getName() + " count: " + count);
+        emptySlots.acquire();
+        System.out.println("Thread " + Thread.currentThread().getName()+ "acquired empty");
         if (frontPointer == -1) {
             frontPointer = 0;
         }
         rearPointer = (rearPointer + 1) % roadSize;
         cars[rearPointer] = car;
+        count++;
         mutex.release();
-        System.out.println("Thread " + Thread.currentThread().getName()+ "released mutex");
+        System.out.println("Trying to release occupied  " + Thread.currentThread().getName() + " count: " + count);
         occupiedSlots.release();
         System.out.println("Thread " + Thread.currentThread().getName()+ "released empty");
         /*
@@ -65,24 +74,20 @@ public class Road {
         * add the car to the array
         * release the mutex*/
     }
-
-
-    //method to remove vehicle (consume)
     public Vehicle removeVehicle() throws InterruptedException{
+        System.out.println("Trying to acquire occupied " + Thread.currentThread().getName() + " count: " + count);
         occupiedSlots.acquire();
         System.out.println("Thread " + Thread.currentThread().getName()+ "acquired occupied");
-        mutex.acquire();
-        System.out.println("Thread " + Thread.currentThread().getName()+ "acquired mutex");
         Vehicle removedCar;
         removedCar = cars[frontPointer];
+        count--;
         if (frontPointer == rearPointer) {
             frontPointer = -1;
             rearPointer = -1;
         } else {
             frontPointer = (frontPointer + 1) % roadSize;
         }
-        mutex.release();
-        System.out.println("Thread " + Thread.currentThread().getName()+ "released mutex");
+        System.out.println("Trying to release empty" + Thread.currentThread().getName() + " count: " + count);
         emptySlots.release();
         System.out.println("Thread " + Thread.currentThread().getName()+ "released empty");
         return removedCar;
